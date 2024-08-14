70a7aa61eeb8d8fe841114bc6d9ea90cbfd1128f
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-08-14 14:26:56 +1000

    added library tests - add and search for book

---
+++ lms/src/test/java/LibraryTest.java
@@ -0,0 +1,26 @@
+package com.lms;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+
+    @Test
+    void testAddBook() {
+        Library library = new Library();
+        Book book = new Book("Effective Java", "Joshua Bloch", "9780134686097");
+        library.addBook(book);
+
+        assertTrue(library.getBooks().contains(book));
+    }
+
+    @Test
+    void testSearchBook() {
+        Library library = new Library();
+        Book book = new Book("Effective Java", "Joshua Bloch", "9780134686097");
+        library.addBook(book);
+
+        Book foundBook = library.searchBook("Effective Java");
+        assertNotNull(foundBook);
+        assertEquals("Effective Java", foundBook.getTitle());
+    }
+}
