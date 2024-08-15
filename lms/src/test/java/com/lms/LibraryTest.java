12e969eabb370c95e89c7771a7b4555228a99958
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-08-15 16:22:57 +1000

    added test for returnBook()

--- lms/src/test/java/com/lms/LibraryTest.java
+++ lms/src/test/java/com/lms/LibraryTest.java
@@ -2,30 +2,42 @@ package com.lms;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
 
 class LibraryTest {
 
-    @Test
-    void testAddBook() {
-        Library library = new Library();
-        Book book = new Book("Effective Java", "Joshua Bloch", "9780134686097",
-                Book.Category.EDUCATION);
-        library.addBook(book);
+    private Library library;
+    private Student student;
+    private Book book;
 
+    @BeforeEach
+    public void setup() {
+        library = new Library();
+        book = new Book("Effective Java", "Joshua Bloch", "9780134686097", Book.Category.EDUCATION);
+        library.addBook(book);
+        student = new Student("Bob", "S001");
+    }
+
+    @Test
+    public void testAddBook() {
         assertTrue(library.getBooks().contains(book));
     }
 
     @Test
-    void testSearchBook() {
-        Library library = new Library();
-        Book book = new Book("Effective Java", "Joshua Bloch", "9780134686097",
-                Book.Category.EDUCATION);
-        library.addBook(book);
-
+    public void testSearchBook() {
         Book foundBook = library.searchBook("Effective Java");
         assertNotNull(foundBook);
         assertEquals("Effective Java", foundBook.getTitle());
     }
 
+    @Test
+    public void testReturnBook() {
+        student.borrowBook(book);
+        assertTrue(book.isBorrowed() != null);
+        student.returnBook(book, LocalDate.now());
+        assertTrue(book.isBorrowed() == null);
+    }
 
 }
