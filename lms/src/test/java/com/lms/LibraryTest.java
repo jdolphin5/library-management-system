a735dbbefb858c6fb4dd35e14c620e536ebdee90
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-08-15 15:46:41 +1000

    updated book instantiation

--- lms/src/test/java/com/lms/LibraryTest.java
+++ lms/src/test/java/com/lms/LibraryTest.java
@@ -8,7 +8,8 @@ class LibraryTest {
     @Test
     void testAddBook() {
         Library library = new Library();
-        Book book = new Book("Effective Java", "Joshua Bloch", "9780134686097");
+        Book book = new Book("Effective Java", "Joshua Bloch", "9780134686097",
+                Book.Category.EDUCATION);
         library.addBook(book);
 
         assertTrue(library.getBooks().contains(book));
@@ -17,11 +18,14 @@ class LibraryTest {
     @Test
     void testSearchBook() {
         Library library = new Library();
-        Book book = new Book("Effective Java", "Joshua Bloch", "9780134686097");
+        Book book = new Book("Effective Java", "Joshua Bloch", "9780134686097",
+                Book.Category.EDUCATION);
         library.addBook(book);
 
         Book foundBook = library.searchBook("Effective Java");
         assertNotNull(foundBook);
         assertEquals("Effective Java", foundBook.getTitle());
     }
+
+
 }
